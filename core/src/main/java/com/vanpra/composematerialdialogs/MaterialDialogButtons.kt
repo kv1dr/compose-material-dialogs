5bd17208fef8eae5af80b86bb1e3f5f481338ea9
Davor Poznič <davor@dat-con.com>
2023-03-03 12:42:51 +0100

    * fixed compile errors

--- core/src/main/java/com/vanpra/composematerialdialogs/MaterialDialogButtons.kt
+++ core/src/main/java/com/vanpra/composematerialdialogs/MaterialDialogButtons.kt
@@ -2,14 +2,13 @@ package com.vanpra.composematerialdialogs
 
 import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.ButtonColors
-import androidx.compose.material.ButtonDefaults
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
